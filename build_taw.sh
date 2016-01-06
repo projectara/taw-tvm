89c1872e0c5ea3aa0ed00f0c8e9546695ac639cb
Eli Sennesh <esennesh@leaflabs.com>
2016-01-06 15:45:45 -0500

    taw-tvm: include printer support in the TAW
    Include printer drivers in the TAW live-image, and code in the TAW dialog
    application to print out PDF reports issued by TVMs.  Analogously to ISOs, PDFs
    are detected by taw.py and the user is prompted to print each one.
    
    Testing Done: print report from bootrom reproduction
    Bug Number: SW-1973
    Review URL:
    Reviewed by:
    
    Signed-off-by: Eli Sennesh <esennesh@leaflabs.com>

--- build_taw.sh
+++ build_taw.sh
@@ -10,11 +10,14 @@ apt-get install --assume-yes live-build live-boot live-config &&
 echo "Installed Debian Live infrastructure [OK]" &&
 mkdir -p taw/image &&
 cd taw/image &&
-lb config --binary-images iso-hybrid &&
+lb config --binary-images iso-hybrid -a i386 &&
 echo "Initialized fresh Debian Live template for TAW [OK]" &&
 cd config &&
 echo "qemu qemu-utils qemu-system python3-dialog xorriso" > package-lists/taw.list.chroot &&
+echo "cups cups-client foomatic-db" > package-lists/printing.list.chroot &&
 echo "task-cinnamon-desktop" > package-lists/desktop.list.chroot &&
+mkdir -p packages.chroot/ &&
+cp ../../custom_packages/* packages.chroot/ &&
 mkdir -p includes.chroot/ &&
 cd includes.chroot/ &&
 cp -R ../../../contents/* . &&
@@ -22,5 +25,5 @@ mkdir -p etc/skel/tashare &&
 cd ../../ &&
 lb build &&
 echo "Built Debian Live ISO for TAW [OK]" &&
-cp live-image-amd64.hybrid.iso "$1" &&
+cp live-image-i386.hybrid.iso "$1" &&
 echo "Wrote Debian Live ISO to USB medium [OK]"
